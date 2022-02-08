cdaa58d98a074da03694dc0469451e003251cfed
Humberto-C <72180767+Humberto-C@users.noreply.github.com>
2022-02-07 23:42:40 -0600

    Codewars challenges monday Week3

---
+++ week3/data/codewars-monday.js
@@ -0,0 +1,83 @@
+// CODEWARS CHALLENGES MONDAY
+
+/*
+1. You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
+
+Implement the function which takes an array containing the names of people that like an item. It must return the display text as shown in the examples:
+
+[]                                -->  "no one likes this"
+["Peter"]                         -->  "Peter likes this"
+["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+Note: For 4 or more names, the number in "and 2 others" simply increases.
+*/
+
+const likes = (names) => {
+  switch(names.length){
+      case 0: return "no one likes this";
+      case 1: return `${names[0]} likes this`;
+      case 2: return `${names[0]} and ${names[1]} like this`;
+      case 3: return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+      default: return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+  }
+}
+
+
+
+/*
+2. Write a function that takes an integer as input, and returns the number of bits that are equal to one in the binary representation of that number. You can guarantee that input is non-negative.
+
+Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case
+*/
+
+
+const countBits = (n) => n.toString(2).replace(/0/g, '').length;
+
+
+
+/*
+3. In this kata you have to write a simple Morse code decoder. While the Morse code is now mostly superseded by voice and digital data communication channels, it still has its use in some applications around the world.
+The Morse code encodes every character as a sequence of "dots" and "dashes". For example, the letter A is coded as ·−, letter Q is coded as −−·−, and digit 1 is coded as ·−−−−. The Morse code is case-insensitive, traditionally capital letters are used. When the message is written in Morse code, a single space is used to separate the character codes and 3 spaces are used to separate words. For example, the message HEY JUDE in Morse code is ···· · −·−−   ·−−− ··− −·· ·.
+
+NOTE: Extra spaces before or after the code have no meaning and should be ignored.
+
+In addition to letters, digits and some punctuation, there are some special service codes, the most notorious of those is the international distress signal SOS (that was first issued by Titanic), that is coded as ···−−−···. These special codes are treated as single special characters, and usually are transmitted as separate words.
+
+Your task is to implement a function that would take the morse code as input and return a decoded human-readable string.
+
+For example:
+
+decodeMorse('.... . -.--   .--- ..- -.. .')
+//should return "HEY JUDE"
+NOTE: For coding purposes you have to use ASCII characters . and -, not Unicode characters.
+
+The Morse code table is preloaded for you as a dictionary, feel free to use it:
+
+Coffeescript/C++/Go/JavaScript/Julia/PHP/Python/Ruby/TypeScript: MORSE_CODE['.--']
+C#: MorseCode.Get(".--") (returns string)
+F#: MorseCode.get ".--" (returns string)
+Elixir: @morse_codes variable (from use MorseCode.Constants). Ignore the unused variable warning for morse_codes because it's no longer used and kept only for old solutions.
+Elm: MorseCodes.get : Dict String String
+Haskell: morseCodes ! ".--" (Codes are in a Map String String)
+Java: MorseCode.get(".--")
+Kotlin: MorseCode[".--"] ?: "" or MorseCode.getOrDefault(".--", "")
+Racket: morse-code (a hash table)
+Rust: MORSE_CODE
+Scala: morseCodes(".--")
+Swift: MorseCode[".--"] ?? "" or MorseCode[".--", default: ""]
+C: provides parallel arrays, i.e. morse[2] == "-.-" for ascii[2] == "C"
+NASM: a table of pointers to the morsecodes, and a corresponding list of ascii symbols
+All the test strings would contain valid Morse code, so you may skip checking for errors and exceptions. In C#, tests will fail if the solution code throws an exception, please keep that in mind. This is mostly because otherwise the engine would simply ignore the tests, resulting in a "valid" solution.
+
+Good luck!
+*/
+
+
+
+
+
+
+
+
+
