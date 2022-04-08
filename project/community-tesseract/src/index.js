68624ca21399a3479bbf37a6e10c73d5fbeccba5
Humberto-C <72180767+Humberto-C@users.noreply.github.com>
2022-04-07 19:50:37 -0600

    Delete project directory

--- project/community-tesseract/src/index.js
+++
@@ -1,9 +0,0 @@
-const Express = require("express")
-const { initializeDB } = require("./lib/db")
-
-const App = Express();
-
-App.listen(3000, () => {
-    console.log("I'M READY :)");
-    initializeDB().then(() => console.log("DB ready :)"));
-})
